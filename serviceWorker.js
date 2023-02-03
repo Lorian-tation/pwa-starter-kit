a81d44a504c6ec89fd91daea191f617c4a459e66
Lorian-tation <99471808+Lorian-tation@users.noreply.github.com>
2023-02-03 21:01:20 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,62 +1,54 @@
-function setCacheForAll() {
-    const cacheVersion = "v1";
-    const assets = ["/", "/index.php"];
-    
-    //Est déclenché lorsque le service worker est installé pour la première fois. 
-    //L'événement "waitUntil" est utilisé pour garantir que toutes les ressources spécifiées 
-    //dans la liste "assets" sont correctement mises en cache avant de considérer l'installation comme terminée.
-    self.addEventListener("install", (e) => {
-      e.waitUntil(
-        caches.open(cacheVersion).then((cache) => {
-          cache.addAll(assets);
-        })
-      );
-    });
-    
-    // Est déclenché lorsque le navigateur effectue une requête de ressources (par exemple, une image ou une page HTML). 
-    // Le code dans cet écouteur vérifie si la ressource demandée est déjà en cache, et si c'est le cas, il la retourne immédiatement. 
-    // Sinon, il effectue une requête réseau pour la ressource et la met en cache pour une utilisation future.
-    self.addEventListener("fetch", (event) => {
-      event.respondWith(
-        caches.match(event.request).then(function (response) {
-          if (response) {
-            return response;
-          }
-    
-          var fetchRequest = event.request.clone();
-    
-          return fetch(fetchRequest).then(function (response) {
-            if (!response || response.status !== 200 || response.type !== "basic") {
-              return response;
-            }
-    
-            var responseToCache = response.clone();
-    
-            caches.open(cacheVersion).then(function (cache) {
-              if((event.request.url.indexOf('http') === 0)){ 
-                      cache.put(event.request, responseToCache);
-                }
-            });
-    
-            return response;
-          });
-        })
-      );
-    });
-    
-    // Est déclenché lorsque le service worker est activé et commence à gérer les requêtes. 
-    // Le code dans cet écouteur efface tous les anciens caches qui ne sont pas celui défini par "cacheVersion".
-    self.addEventListener("activate", (e) => {
-      e.waitUntil(
-        caches.keys().then((keys) => {
-          return Promise.all(
-            keys
-              .filter((key) => key !== cacheVersion)
-              .map((key) => caches.delete(key))
-          );
-        })
-      );
-    });    
-}
+const PREFIX = 'V1';
 
-setCacheForAll();
+self.addEventListener('install', (event) => {
+    // Arrête le service worker précédent pour activer la nouvelle version
+    self.skipWaiting();
+
+    // Lecture de la page offline et mémorisation en cache
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(PREFIX);
+            cache.add(new Request ('/offline.html'));
+        })()
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    // Active et fait fonctionner le service worker directement lors de la première visite
+    clients.claim();
+
+    // Vide les caches différents de la version actuelle (PREFIX)
+    event.waitUntil(
+        (async() => {
+            const keys = await caches.keys();
+            await Promise.all(
+                keys.map(key => {
+                    if(!key.includes(PREFIX)){
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })()
+    );
+});
+
+// Le navigateur renvoie la page offline.html lorsqu'il n'est pas en mesure de se connecter au serveur
+self.addEventListener("fetch", (event) => {
+    if(event.request.mode == 'navigate'){
+        event.respondWith(
+            (async () => {
+                try{
+                    const preloadResponse = await event.preloadResponse;
+                    if(preloadResponse){
+                        return preloadResponse;
+                    }
+                    return await fetch(event.request);
+                } catch (e) {
+                    // Si l'utilisateur est hors-ligne, on lit le fichier offline du cache
+                    const cache = await caches.open(PREFIX);
+                    return await cache.match('/offline.html');
+                }
+            })()
+        );
+    }
+});
